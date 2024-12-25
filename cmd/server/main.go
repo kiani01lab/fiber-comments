b90b4060169df64c708cde20c3c8eccae50386f7
ali <codewithkiani@gmail.com>
2024-12-25 08:45:25 +0330

    Evaluate viper

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,16 +2,21 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
+	"strings"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/kiani01lab/fiber-comments/cmd/handlers"
 	"github.com/kiani01lab/fiber-comments/db"
+	"github.com/spf13/viper"
 )
 
 func main() {
 	listenAddr := flag.String("listernAddress", ":8000", "the listen address of the servie")
 	flag.Parse()
+
+	viperEvaluated()
 
 	app := fiber.New()
 	api := app.Group("/api")
@@ -28,4 +33,17 @@ func main() {
 	api.Get("/user/:id", userHandler.HandleGetUser)
 	api.Delete("/user/:id", userHandler.HandleDeleteUser)
 	app.Listen(*listenAddr)
+}
+
+func viperEvaluated() {
+	viper.SetConfigFile("config")
+	viper.SetConfigType("json")
+	viper.AddConfigPath(".")
+	viper.AutomaticEnv()
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+
+	if err := viper.ReadInConfig(); err != nil {
+		panic(fmt.Errorf("unable to evaluate Viper: %w", err))
+	}
+	log.Println("Viper config evaluated")
 }
