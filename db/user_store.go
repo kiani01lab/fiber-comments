b4ccafc81a17776de5da69beb6706370baa367e8
kiani <codewithkiani@gmail.com>
2025-01-27 16:14:00 +0330

    add GetUserByEmail method

--- db/user_store.go
+++ db/user_store.go
@@ -15,6 +15,7 @@ const userColl = "users"
 type UserStore interface {
 	GetUsers(context.Context) ([]*types.User, error)
 	GetUserById(context.Context, string) (*types.User, error)
+	GetUserByEmail(context.Context, string) (*types.User, error)
 	InsertUser(context.Context, *types.User) (*types.User, error)
 	DeleteUser(context.Context, string) error
 }
@@ -39,6 +40,15 @@ func (s *MongoUserStore) GetUserById(ctx context.Context, id string) (*types.Use
 
 	var user types.User
 	if err := s.coll.FindOne(ctx, bson.M{"_id": oid}).Decode(&user); err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func (s *MongoUserStore) GetUserByEmail(ctx context.Context, email string) (*types.User, error) {
+	var user types.User
+	if err := s.coll.FindOne(ctx, bson.M{"email": email}).Decode(&user); err != nil {
 		return nil, err
 	}
 
